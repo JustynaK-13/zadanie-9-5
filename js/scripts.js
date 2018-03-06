f558811d3a4010bd8b645551d7a0c713025efc54
Justyna Kaczorowska <js.kaczorowska@gmail.com>
2018-03-06 15:54:50 +0100

    Change content

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 var buttonClass = document.getElementsByClassName('button');
 var i;
-
 for ( i = 0 ; i < buttonClass.length ; i++) {
   console.log('The value of buttonClass is now: ' + i);
   alert(buttonClass[i].innerText );
