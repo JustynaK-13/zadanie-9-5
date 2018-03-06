b61db84f2b1d98d70e7f171f054d2373b68f8719
Justyna Kaczorowska <js.kaczorowska@gmail.com>
2018-03-06 15:52:26 +0100

    Delete: console.log(buttonClass[i])

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 var buttonClass = document.getElementsByClassName('button');
 var i;
-  console.log(buttonClass[i]);
 
 for ( i = 0 ; i < buttonClass.length ; i++) {
   console.log('The value of buttonClass is now: ' + i);
