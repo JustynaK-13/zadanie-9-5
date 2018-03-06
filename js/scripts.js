dd624fbb6d18f6a25c96b1612e01662107b02964
Justyna Kaczorowska <js.kaczorowska@gmail.com>
2018-03-06 10:50:28 +0100

    Add var to buttonClass

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 var buttonClass = document.getElementsByClassName('button');
-  console.log(buttonClass);
-
 var i;
+  console.log(buttonClass[i]);
 
 for ( i = 0 ; i < buttonClass.length ; i++) {
   console.log('The value of buttonClass is now: ' + i);
-  alert(buttonClass.innerText);
+  alert(buttonClass[i].innerText );
 }
